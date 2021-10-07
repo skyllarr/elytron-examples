777935afe24601262f8c4549949bdad08ddde183
Diana Krepinska Vilkolakova <skyllarr@gmail.com>
2021-10-07 12:02:01 +0200

    demo

--- resteasy-client-integration/client/src/test/java/Client.java
+++ resteasy-client-integration/client/src/test/java/Client.java
@@ -1,4 +1,3 @@
-import org.apache.http.conn.ssl.NoopHostnameVerifier;
 import org.jboss.resteasy.client.jaxrs.ResteasyClient;
 import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
 import org.junit.Assert;
@@ -15,11 +14,11 @@ public class Client {
 
     @Test
     public void test() {
-        ResteasyClient client = new ResteasyClientBuilder().hostnameVerifier(NoopHostnameVerifier.INSTANCE).build();
+        ResteasyClient client = new ResteasyClientBuilder().build();
         Response response = client.target("https://127.0.0.1:8443/resteasy-client-integration-example/rest/hello").request().get();
         Assert.assertNotNull(response);
-        Assert.assertEquals(response.getStatus(), 200);
-        Assert.assertEquals(response.readEntity(String.class), "Hello jane");
+        Assert.assertEquals(200, response.getStatus());
+        Assert.assertEquals("Hello jane", response.readEntity(String.class));
         System.out.println("\n\nRESTEasy client have successfully used SSLContext and credentials from Elytron client to authenticate.\n\n");
     }
 }
